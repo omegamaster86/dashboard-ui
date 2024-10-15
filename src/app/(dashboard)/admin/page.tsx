d3129064187af6b81c58ac815a3e5b508b42dd70
omegamaster86 <masya868686@gmail.com>
2024-10-15 23:20:56 +0900

    カードを追加

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,22 @@
+import UserCard from '@/components/userCard'
 import React from 'react'
 
 const Adminpage = () => {
   return (
-    <div>Admin</div>
+    <div className='p-4 flex flex-col gap-4 md:flex-row'>
+      {/* left */}
+      <div className='w-full lg:w-2/3'>
+        {/* userCard */}
+        <div className='flex gap-4 justify-between'>
+          <UserCard type='student'/>
+          <UserCard type='teacher'/>
+          <UserCard type='parent'/>
+          <UserCard type='staff'/>
+        </div>
+      </div>
+      {/* right */}
+      <div className='w-full lg:w-1/3'>r</div>
+    </div>
   )
 }
 
