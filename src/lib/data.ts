37ec322ee7d9ace0cc4558fd718dcff6843096c9
omegamaster86 <masya868686@gmail.com>
2024-10-28 10:03:41 +0900

    calendarEventsの日付を本日に変更

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -921,8 +921,8 @@ export const calendarEvents = [
   {
     title: "Math",
     allDay: false,
-    start: new Date(2024, 7, 12, 8, 0),
-    end: new Date(2024, 7, 12, 8, 45),
+    start: new Date(2024, 10, 27, 8, 0),
+    end: new Date(2024, 10, 27, 8, 45),
   },
   {
     title: "English",
