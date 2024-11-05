96c481c4c1bd4fec0b2ecf5e8284eeffe297bbd8
omegamaster86 <masya868686@gmail.com>
2024-11-05 09:41:57 +0900

    一旦コミット

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Announcements from "@/components/announcements";
+import BigCalendar from "@/components/bigCalender";
 
-const Parentpage = () => {
+const ParentPage = () => {
   return (
-    <div>Parent</div>
-  )
-}
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default Parentpage
+export default ParentPage;
